c29f20743404705d3faa998262ca180b42dbf315
tanvir-lnx <tanvir.animation@gmail.com>
2025-08-08 10:21:48 +0600

    create: taskmodel

---
+++ lib/data/models/task_model.dart
@@ -0,0 +1,42 @@
+/*
+ "_id": "688fd398b4f34e7d4b426d48",
+  "title": "dsadasd",
+  "description": "sadasdas",
+  "status": "New",
+  "email": "tanvir.animation@gmail.com",
+  "createdDate": "2025-07-16T06:07:55.798Z"
+*/
+
+class TaskModel {
+  final String id;
+  final String title;
+  final String description;
+  final String status;
+  final String createdDate;
+
+  TaskModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.status,
+    required this.createdDate,
+  });
+
+  factory TaskModel.fromJson(Map<String, String> jsonTask) {
+    return TaskModel(
+      id: jsonTask['_id']!,
+      title: jsonTask['title']!,
+      description: jsonTask['description']!,
+      status: jsonTask['status']!,
+      createdDate: jsonTask['createdDate']!,
+    );
+  }
+
+  Map<String, String> toJson() {
+    return {
+      'title': title,
+      'description': description,
+      'createdDate': createdDate,
+    };
+  }
+}
