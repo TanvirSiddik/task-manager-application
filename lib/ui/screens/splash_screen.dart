8ee7923c8ea2c01962de7c98166e1ac0ae6173b3
tanvir-lnx <tanvir.animation@gmail.com>
2025-08-08 09:52:26 +0600

    update: ui for unauth user

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:taskmanger_no_getx/controller/auth_controller.dart';
+import 'package:taskmanger_no_getx/data/network/network_caller.dart';
+import 'package:taskmanger_no_getx/data/utils/api_config.dart';
 import 'package:taskmanger_no_getx/ui/screens/login_screen.dart';
 import 'package:taskmanger_no_getx/ui/screens/main_nav_bar_screen.dart';
 
@@ -18,13 +20,23 @@ class _SplashScreenState extends State<SplashScreen> {
   Future<void> _leaveSplashScreen() async {
     await Future.delayed(Duration(seconds: 2));
     if (!mounted) return;
-    if (await AuthController.isUserLoggedIn()) {
+    if (await AuthController.isUserLoggedIn() && await _userAuthStatus()) {
       if (!mounted) return;
       Navigator.pushReplacementNamed(context, MainNavBarScreen.name);
     } else {
       if (!mounted) return;
       Navigator.pushReplacementNamed(context, LoginScreen.name);
     }
+  }
+
+  Future<bool> _userAuthStatus() async {
+    NetworkResponse response = await NetworkCaller.getRequest(
+      url: ApiConfig.profileDetails,
+    );
+    if (response.body['status'] == 'unauthorized') {
+      return false;
+    }
+    return true;
   }
 
   @override
